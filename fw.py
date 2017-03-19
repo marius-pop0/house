41e2dfe3d516a8acdb9c29ce66e8e0f238aca621
Marius <marius.pop94@gmail.com>
2017-03-18 21:32:20 -0600

    packet breakup and validation!

--- fw.py
+++ fw.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         print("wrong number of arguments")
         sys.exit(0)
 
-    #reading rules
+    #############reading rules
     try:
         file_object=open(ruleFile,'r')
         line_count = 1
@@ -31,16 +31,49 @@ if __name__ == '__main__':
         file_object.close()
     except FileNotFoundError:
         print("Could not find rule file")
+    ##############
 
-
-    #reading packets from standard input
+    ###########reading packets from standard input
     for line2 in sys.stdin:
         line2_split = line2.split()
         if (len(line2_split) == 4):
             print("Direction: " + line2_split[0] + " IP: " + line2_split[1] + " Port: " + line2_split[2] + " Flag: " +line2_split[3])
+            dir=""
+            ip=""
+            port=-1
+            flag=-1
+
+            if(line2_split[0]=="in" or line2_split[0]=="out"):
+                dir=line2_split[0]
+            else:
+                print("Invalid packet direction")
+                sys.exit(0)
+
+            ip=line2_split[1]
+            ip_split=ip.split(".")
+            if(len(ip_split)!=4):
+                print ("invalid IP adress in packet")
+                sys.exit(0)
+            else:
+                ip_1='{0:08b}'.format(int(ip_split[0]))
+                ip_2 = '{0:08b}'.format(int(ip_split[1]))
+                ip_3 = '{0:08b}'.format(int(ip_split[2]))
+                ip_4 = '{0:08b}'.format(int(ip_split[3]))
+
+                ip=ip_1+ip_2+ip_3+ip_4
+
+            if(int(line2_split[2])>=1 or int(line2_split[2])<=65535):
+                port=int(line2_split[2])
+            else:
+                print("invalid port number in packet")
+                sys.exit(0)
+            if (int(line2_split[3]) == 0 or int(line2_split[3]) == 1):
+                flag = int(line2_split[3])
+            else:
+                print("Invalid packet flag")
+                sys.exit(0)
+
         else:
             print("wrong line format on line in packet")
             sys.exit(0)
-
-    #read from standard input
-    #sys.stdin.buffer.read(BUFFER_SIZE - 16)
+    ###############
