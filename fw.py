72f7ec8eb0ae531ebd50ac52893fec3a753dfc58
Marius <marius.pop94@gmail.com>
2017-03-19 17:39:50 -0600

    All Error checking done on both the packets and the rules files

--- fw.py
+++ fw.py
@@ -1,5 +1,91 @@
-import string, sys
+import sys
 
+def findMatch(dir,ip,port,flag):
+    #############reading rules
+    line_count = 1
+    try:
+        file_object = open(ruleFile, 'r')
+        for line in file_object:
+            #print("line #" + str(line_count) + " : " + line)
+
+            line_split = line.split()
+            rule_dir = ""
+            rule_action = ""
+            rule_ip = ""
+            rule_subnet_size=0
+            rule_ports = []
+            rule_flag = ""
+
+            # does not contain flag
+            if (len(line_split) == 4):
+                pass
+            # line contains flag
+            elif (len(line_split) == 5):
+                if(line_split[4]=="established"):
+                    rule_flag=line_split[4]
+                else:
+                    print("Invalid flag in rule "+line_count)
+                    sys.exit(0)
+            # error on reading line (formatting)
+            else:
+                print("wrong line format on line in rule " + line_count)
+                sys.exit(0)
+
+            #get rule dir
+            if (line_split[0] == "in" or line_split[0] == "out"):
+                rule_dir = line_split[0]
+            else:
+                print("Invalid direction in rule "+line_count)
+                sys.exit(0)
+
+            #get rule action
+            if (line_split[1] == "accept" or line_split[1] == "drop" or line_split[1] == "deny"):
+                rule_action = line_split[1]
+            else:
+                print("Invalid action in rule "+line_count)
+                sys.exit(0)
+
+            #get rule IP/subnet mask
+            rule_ip = line_split[2]
+            ip_subnet=rule_ip.split("/")
+            if(len(ip_subnet)==2):
+                ip_split=ip_subnet[0].split(".")
+                if (len(ip_split) == 4):
+                    ip_1 = '{0:08b}'.format(int(ip_split[0]))
+                    ip_2 = '{0:08b}'.format(int(ip_split[1]))
+                    ip_3 = '{0:08b}'.format(int(ip_split[2]))
+                    ip_4 = '{0:08b}'.format(int(ip_split[3]))
+                    #ip in binary
+                    rule_ip = ip_1 + ip_2 + ip_3 + ip_4
+                else:
+                    print("invalid ip at rule "+line_count)
+                rule_subnet_size=int(ip_subnet[1])
+
+            #get rule port(s)
+            rule_port=line_split[3]
+            rule_ports=rule_port.split(",")
+            ##error check
+            for ports in rule_ports:
+                if (ports=="*"):
+                    pass
+                elif (int(ports)< 0 or int(ports) > 65535):
+                    print("invalid port number at rule "+line_count)
+
+
+            ##what to do when packet matches
+            ###add here###
+            ######
+
+            print("Direction: " + rule_dir + " Action: " + rule_action + " IP: " + rule_ip + " Subnet Size: "+str(rule_subnet_size) + " Port: " +
+              str(rule_ports) + " Flag: " + rule_flag)
+
+            line_count += 1
+            ##############
+        file_object.close()
+
+    except FileNotFoundError:
+        print("Could not find rule file")
+        sys.exit(0)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
@@ -8,41 +94,10 @@ if __name__ == '__main__':
         print("wrong number of arguments")
         sys.exit(0)
 
-    #############reading rules
-    try:
-        file_object=open(ruleFile,'r')
-        line_count = 1
-        for line in file_object:
-            print("line #" + str(line_count) + " : " + line)
-            line_split=line.split()
-            dir=""
-            action=""
-            ip=""
-            port=-1
-            flag=""
-
-            #does not contain flag
-            if (len(line_split)==4):
-                print("Direction: "+line_split[0]+" Action: "+line_split[1]+" IP: "+line_split[2]+" Port: "+line_split[3])
-            #line contains flag
-            elif (len(line_split)==5):
-                print("Direction: "+line_split[0]+" Action: "+line_split[1]+" IP: "+line_split[2]+" Port: "+line_split[3]+" Flag: "+line_split[4])
-            #error on reading line (formatting)
-            else:
-                print("wrong line format on line in rule "+line_count)
-                sys.exit(0)
-            line_count += 1
-
-        file_object.close()
-    except FileNotFoundError:
-        print("Could not find rule file")
-    ##############
-
     ###########reading packets from standard input
     for line2 in sys.stdin:
         line2_split = line2.split()
         if (len(line2_split) == 4):
-            print("Direction: " + line2_split[0] + " IP: " + line2_split[1] + " Port: " + line2_split[2] + " Flag: " +line2_split[3])
             dir=""
             ip=""
             port=-1
@@ -68,7 +123,7 @@ if __name__ == '__main__':
                 print("invalid IP address in packet")
                 sys.exit(0)
 
-            if(int(line2_split[2])>=1 and int(line2_split[2])<=65535):
+            if(int(line2_split[2])>=0 and int(line2_split[2])<=65535):
                 port=int(line2_split[2])
             else:
                 print("invalid port number in packet")
@@ -79,7 +134,12 @@ if __name__ == '__main__':
                 print("Invalid packet flag")
                 sys.exit(0)
 
+            print("Direction: " + dir + " IP: " + ip + " Port: " + str(port) + " Flag: " +
+                  str(flag))
+            findMatch(dir,ip,port,flag)
+
         else:
             print("wrong line format on line in packet")
             sys.exit(0)
     ###############
+
